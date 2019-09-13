e5d69e00a47210525380ae070693f565e2443d84
itsalimanuel <itskhaloufali@gmail.com>
2019-09-13 15:51:15 +0300

    Updates

---
+++ precache-manifest.137bed8f1c8969af031ce6f554355bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1953db79cd1c0e95bbbe7e6294a7370",
+    "url": "/ai/index.html"
+  },
+  {
+    "revision": "92208fbb655ab8ec635c",
+    "url": "/ai/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "871973f768083225fae4",
+    "url": "/ai/static/css/main.ffcb955f.chunk.css"
+  },
+  {
+    "revision": "92208fbb655ab8ec635c",
+    "url": "/ai/static/js/2.b4b07c21.chunk.js"
+  },
+  {
+    "revision": "871973f768083225fae4",
+    "url": "/ai/static/js/main.f342956d.chunk.js"
+  },
+  {
+    "revision": "e3de631928c3b10b0f73",
+    "url": "/ai/static/js/runtime~main.7881749c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ai/static/media/logo.5d5d9eef.svg"
+  }
+]);
